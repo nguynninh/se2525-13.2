c004d9b8a45eecd7bfb98961114dc33286d78c69
Nguyen Ninh <nguyenvanninh2004pt@gmail.com>
2025-10-10 05:06:38 +0700

    feat: Enhance login functionality with validation and internationalization support

--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,42 +1,125 @@
-import { View, Text } from 'react-native';
-import { ButtonComponent, InputComponent } from '../../components';
-import { appColors } from '../../constants/appColors';
+import { Image, Switch } from 'react-native';
+import { useState } from 'react';
 import { Lock, Sms } from 'iconsax-react-native';
+import {
+  ButtonComponent,
+  ContainerComponent,
+  InputComponent,
+  RowComponent,
+  SectionComponent,
+  SpaceComponent,
+  TextComponent,
+} from '../../components';
+import { appColors } from '../../constants/appColors';
+import { Validate } from '../../utils/validate';
+import { useTranslation } from 'react-i18next';
+import { Login } from '../../models/Login';
 
-const LoginScreen = () => {
+const LoginScreen = ({ navigation }: any) => {
+  const { t } = useTranslation('auth');
+
+  const [data, setData] = useState<Login>({ email: '', password: '', isRemember: true });
+  const [valiationError, setValidationError] = useState({ email: '', password: '' });
+
+  const validationErrorHandler = () => {
+    const newError = { email: '', password: '' };
+
+    if (!data.email) {
+      newError.email = t('auth:email_required');
+    } else if (!Validate.email(data.email)) {
+      newError.email = t('auth:email_format_invalid');
+    }
+
+    if (!data.password) {
+      newError.password = t('auth:password_required');
+    } else if (!Validate.password(data.password)) {
+      newError.password = t('auth:password_invalid');
+    }
+
+    setValidationError(newError);
+    return newError.email !== '' || newError.password !== '';
+  };
+
+  const handleLogin = async () => {
+    if (validationErrorHandler()) {return;}
+  };
+
   return (
-    <View style={{flex: 1, justifyContent: 'center'}}>
-      <Text style={{
-        flex: 0,
-        fontSize: 24,
-        fontWeight: '600',
-        marginBottom: 24,
-        textAlignVertical: 'center',
-        textAlign: 'center',
-      }}>LoginScreen</Text>
-
-      <InputComponent
-        value={''}
-        placeholder="Email"
-        onChange={() => {}}
-        allowClear
-        affix={<Sms size={22} color={appColors.gray} />}
-      />
-      <InputComponent
-        value={''}
-        placeholder="Password"
-        onChange={() => {}}
-        isPassword
-        allowClear
-        affix={<Lock size={22} color={appColors.gray} />}
-      />
-      <ButtonComponent
-          disable={false}
-          onPress={() => {}}
-          text="SIGN IN"
+    <ContainerComponent isImageBackground isScroll>
+      <SectionComponent
+        styles={{
+          justifyContent: 'center',
+          alignItems: 'center',
+          marginTop: 75,
+        }}>
+        <Image
+          source={require('../../assets/images/logo.png')}
+          style={{
+            width: 80,
+            height: 100,
+            marginBottom: 30,
+          }}
+        />
+      </SectionComponent>
+      <SectionComponent>
+        <TextComponent size={24} title text={t('auth:login')} />
+        <SpaceComponent height={21} />
+        <InputComponent
+          value={data.email}
+          placeholder={t('auth:email')}
+          error={valiationError.email}
+          onChange={val => setData(prev => ({ ...prev, email: val }))}
+          allowClear
+          affix={<Sms size={22} color={appColors.gray} />}
+          style={{ marginBottom: 19 }}
+        />
+        <InputComponent
+          value={data.password}
+          placeholder={t('auth:password')}
+          error={valiationError.password}
+          onChange={val => setData(prev => ({ ...prev, password: val }))}
+          isPassword
+          allowClear
+          affix={<Lock size={22} color={appColors.gray} />}
+          style={{ marginBottom: 19 }}
+        />
+        <RowComponent justify="space-between">
+          <RowComponent onPress={() => setData(prev => ({ ...prev, isRemember: !prev.isRemember }))}>
+            <Switch
+              trackColor={{ true: appColors.primary }}
+              thumbColor={appColors.white}
+              value={data.isRemember}
+              onChange={() => setData(prev => ({ ...prev, isRemember: !prev.isRemember }))}
+            />
+            <SpaceComponent width={4} />
+            <TextComponent text={t('auth:remember_me')} />
+          </RowComponent>
+          <ButtonComponent
+            text={t('auth:forgot_password')}
+            onPress={() => navigation.navigate('ForgotPassword')}
+            type="text"
+          />
+        </RowComponent>
+      </SectionComponent>
+      <SpaceComponent height={16} />
+      <SectionComponent>
+        <ButtonComponent
+          onPress={handleLogin}
+          text={t('auth:btn_login')}
           type="primary"
         />
-    </View>
+      </SectionComponent>
+      <SectionComponent>
+        <RowComponent justify="center">
+          <TextComponent text={t('auth:no_account')} />
+          <ButtonComponent
+            type="link"
+            text={t('auth:sign_up')}
+            onPress={() => navigation.navigate('SignUpScreen')}
+          />
+        </RowComponent>
+      </SectionComponent>
+    </ContainerComponent>
   );
 };
 

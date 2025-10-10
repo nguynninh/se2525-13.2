764f7ea52452efd3412ae4dd0cd67b806ec4a6d4
Nguyen Ninh <nguyenvanninh2004pt@gmail.com>
2025-10-10 15:53:36 +0700

    feat: Add Forgot Password screen with email validation and localization support

--- src/navigators/AuthNavigator.tsx
+++ src/navigators/AuthNavigator.tsx
@@ -1,5 +1,6 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import {
+    ForgotPassword,
     LoginScreen,
     OnboardingScreen,
     SignUpScreen,
@@ -16,6 +17,7 @@ const AuthNavigator = () => {
             <Stack.Screen name="OnboardingScreen" component={OnboardingScreen} />
             <Stack.Screen name="LoginScreen" component={LoginScreen} />
             <Stack.Screen name="SignUpScreen" component={SignUpScreen} />
+            <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
         </Stack.Navigator>
     );
 };

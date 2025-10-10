80b82b89539fb8f9fbdbe6dd02d4471012ce2f36
Nguyen Ninh <nguyenvanninh2004pt@gmail.com>
2025-10-10 15:26:33 +0700

    feat: Implement Sign Up screen with validation and localization support

---
+++ src/models/SignUp.ts
@@ -0,0 +1,6 @@
+export interface SignUp {
+    fullname: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+}
--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,11 +1,13 @@
 import HomeScreen from './home/HomeScreen';
 import LoginScreen from './auth/LoginScreen';
+import SignUpScreen from './auth/SignUpScreen';
 import SplashScreen from './SplashScreen';
 import OnboardingScreen from './auth/OnboardingScreen';
 
 export {
     HomeScreen,
     LoginScreen,
+    SignUpScreen,
     SplashScreen,
     OnboardingScreen,
 };

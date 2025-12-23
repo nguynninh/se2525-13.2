01170bb9cf1b02f9f6697021bf05c6256389155e
Nguyen Ninh <nguyenvanninh2004pt@gmail.com>
2025-12-23 14:51:25 +0700

    feat: Initialize a new TypeScript Express server with basic configuration and a health check endpoint.

---
+++ server/src/app.ts
@@ -0,0 +1,23 @@
+import { time } from 'console';
+import express from 'express';
+import type { Request, Response } from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/healthy', (req: Request, res: Response) => {
+    res.status(200).json({
+        code: 200,
+        message: 'OK',
+    });
+});
+
+app.use((req: Request, res: Response) => {
+    res.status(404).json({
+        code: 404,
+        message: 'Not Found',
+    });
+});
+
+export default app;

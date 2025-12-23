d8652628e2ac701ca34f54bd993191ec2b27697f
Nguyen Ninh <nguyenvanninh2004pt@gmail.com>
2025-12-23 15:07:02 +0700

    feat: Implement internationalization, configurable API base path, and update build configuration to CommonJS.

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,23 +1,27 @@
-import { time } from 'console';
 import express from 'express';
+import i18nMiddleware from "./i18n";
 import type { Request, Response } from 'express';
+import config from './config/config';
 
 const app = express();
 
 app.use(express.json());
+app.use(i18nMiddleware);
 
-app.get('/healthy', (req: Request, res: Response) => {
-    res.status(200).json({
-        code: 200,
-        message: 'OK',
-    });
+const base = config.apiBasePath || '';
+
+app.get(`${base}/healthy`, (req: Request, res: Response) => {
+	res.status(200).json({
+		code: 200,
+        message: req.t('common:success'),
+	});
 });
 
 app.use((req: Request, res: Response) => {
-    res.status(404).json({
-        code: 404,
-        message: 'Not Found',
-    });
+	res.status(404).json({
+		code: 404,
+        message: req.t('common:error'),
+	});
 });
 
 export default app;
